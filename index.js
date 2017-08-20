df757310da7843594775d8ec113e0289c84c6efb
Marc Andrew <andrewmacatulad@gmail.com>
2017-08-20 18:30:16 +0800

    Initial Commit

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
 	res.send('Fuck You')
 })
 
-app.listen(3000, () => {
+
+app.listen(port, () => {
 	console.log('Server Started at port 3000')
 })
